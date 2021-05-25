9de20a5036e7bded625793f7c8271f5ee6686500
Serg Nikolaev <kndnou@yandex.ru>
2021-05-25 22:29:39 +1100

    created list of dicts and implemented export to file logic

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,21 +1,36 @@
+import csv
 """
 
 Домашнее задание №2
 
 Работа csv
 
-1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору. 
+[x] 1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору. 
    В списке нужно создать не менее 4-х словарей
-2. Запишите содержимое списка словарей в файл в формате csv
+[x] 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+
+LIST_OF_DICTS = [
+    {'name': 'Robert', 'age': 12, 'job': 'student'},
+    {'name': 'Martin', 'age': 23, 'job': 'intern'},
+    {'name': 'Katey', 'age': 31, 'job': 'programmer'},
+    {'name': 'Darleen', 'age': 40, 'job': 'pilot'},
+]
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('export.csv', 'w', encoding='utf-8', newline='') as f:
+        fields = list(LIST_OF_DICTS[0].keys())
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        # writer.writerows(LIST_OF_DICTS)
+        for row in LIST_OF_DICTS:
+            writer.writerow(row)
+    # print(list(LIST_OF_DICTS[0].keys()))
 
 if __name__ == "__main__":
     main()
